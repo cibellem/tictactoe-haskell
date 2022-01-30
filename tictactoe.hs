07aad72d16fc5fbb6a8694373fe45a42fa7c84ec
Cibelle Montor <montorcibelle@gmail.com>
2022-01-30 15:05:33 -0700

    Checks if correct value is entered to start the game

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 -- defines interfaces and behaviours of our data
 type Turn = Int
 
@@ -13,7 +12,6 @@ type Option = Char
 -- Recursion happens a lot in Haskell because there are no loops
 runGame :: Board -> Turn -> IO ()
 runGame slots turn = do
-  print slots
   if turn == 0
     then do
       putStrLn "Player X turn"
@@ -37,18 +35,18 @@ runGame slots turn = do
           runGame slots 1
         else runGame (newBoard slots turn pos) 0
 
-
 --Receives a board a player's turn and a position on the board.
 -- It returns a new board with the position
--- Here we use guards | and otherwise. They are a syntax sugar for if/else statements. Otherwise workds as a  "default" expression
+-- Here we use guards | and otherwise. They are a syntax sugar for if/else statements. Otherwise works as a "default" expression
 -- if the 2 conditions are not met we call again newBoard but this time without the first index;
--- we recursively do it untill one of the conditions are met and then we generate a board with X or O applied to it.
+-- we recursively do it untill one of the conditions are met and then we generate a board with X or O applied to the head.
 newBoard :: Board -> Turn -> Option -> Board
 newBoard (x : xs) turn pos -- x is the first index of the list xs is the rest of the list ['1', '2', '3', '4', '5', '6', '7', '8', '9']
- -- It reads like: Is the first index(head) of slots ['1', '2', '3', '4', '5', '6', '7', '8', '9'] in this case "1" equal to the value of pos and is the turn  equal to 0?
-  | (x == pos) && (turn == 0)  = ['X'] ++ xs
+-- It reads like: Is the first index(head) of slots ['1', '2', '3', '4', '5', '6', '7', '8', '9'] in this case "1" equal to the value of pos and is the turn  equal to 0?
+  | (x == pos) && (turn == 0) = ['X'] ++ xs
   | (x == pos) && (turn == 1) = ['O'] ++ xs
-  | otherwise = x : newBoard xs turn pos  
+  | otherwise = x : newBoard xs turn pos
+
 
 -- The I/O action allow us to read inputs from the user and print things the screen.
 -- Greets player and start/end game
@@ -56,9 +54,10 @@ main :: IO ()
 main = do
   -- "do" glue together several I/O actions into one.
   putStrLn "Welcome! This is Tic Tac Toe built in Haskell :)\n" -- this is an I/O action.
-  putStrLn "Press Y to start!\n"
-  putStrLn "Type 'exit' at any time to quit.\n"
+  putStrLn "Press 1 to start!\n"
   input <- getLine -- "getLine" is an I/O action that contains a result type of String.   <- is used to bind the results of getline to names.
-  if input == "exit"
-    then return ()
-    else runGame ['1', '2', '3', '4', '5', '6', '7', '8', '9'] 0 -- The very first game starts with player 0
+  if input == "1"
+    then runGame ['1', '2', '3', '4', '5', '6', '7', '8', '9'] 0 -- The very first game starts with player 0
+    else do
+      putStrLn "Invalid caracter try again"
+      main
