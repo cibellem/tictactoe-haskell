626a87370dd84e3e913499aa8f3a909a015a60ac
Cibelle Montor <montorcibelle@gmail.com>
2022-01-30 14:07:33 -0700

    Adds comments

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,20 +1,4 @@
-{-
-Tic Tat Toe Game
-Written in Haskell - Vanilla Haskell should be all you need but in that, you could include things from ‘Data’ and ‘System.IO’
-Create a file - Create a file of your exercise that compiles. You can use any editor you prefer
-Done in the CLI - All input/output should be done in the terminal. Display everything in the CLI
-2 manual players - There’s no need to use AI, 2 humans (or animals) will be the ones to play the game
--}
-
---Cibelle MVP
--- When script starts users can choose to start or quit the game at any time
--- A visual representation of the board is displayed in the CLI
---
-
--- Good practice to use type declaration with our functions
--- doubleMe :: Int -> Int
--- doubleMe x = x + x
-
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 -- defines interfaces and behaviours of our data
 type Turn = Int
 
@@ -22,38 +6,49 @@ type Board = String
 
 type Option = Char
 
--- runGame is a recursive function that takes in the Board  and the players turn.
+--Player 0 -> X
+--Player 1 -> O
+
+-- runGame is a recursive function that takes in the Board and the Players turn.
 -- Recursion happens a lot in Haskell because there are no loops
--- A new board is generated after the player picks a position on the board.
 runGame :: Board -> Turn -> IO ()
 runGame slots turn = do
+  print slots
   if turn == 0
     then do
       putStrLn "Player X turn"
       putStrLn ("\n" ++ "" ++ show (slots !! 0) ++ "|" ++ show (slots !! 1) ++ "|" ++ show (slots !! 2) ++ "\n------------\n" ++ "" ++ show (slots !! 3) ++ "|" ++ show (slots !! 4) ++ "|" ++ show (slots !! 5) ++ "\n------------\n" ++ "" ++ show (slots !! 6) ++ "|" ++ show (slots !! 7) ++ "|" ++ show (slots !! 8) ++ "\n")
       pos <- getChar
       getChar -- By doing it the enter was not counted as an user input
-      if notElem pos slots then do --the position is already taken we call runGame again with the same board
-        putStrLn "Opsi option taken, try again!"
-        runGame slots 0
-        else runGame (newBoard slots turn pos) 1
+      if notElem pos slots
+        then do
+          --the position is already taken we call runGame again with the same board
+          putStrLn "Opsi option taken, try again!"
+          runGame slots 0
+        else runGame (newBoard slots turn pos) 1 -- the position is available we call new board to generate a new board. 1 is passed to indicate whose turn is on the next call to runGame
     else do
       putStrLn "Player O turn"
       putStrLn ("\n" ++ "" ++ show (slots !! 0) ++ "|" ++ show (slots !! 1) ++ "|" ++ show (slots !! 2) ++ "\n------------\n" ++ "" ++ show (slots !! 3) ++ "|" ++ show (slots !! 4) ++ "|" ++ show (slots !! 5) ++ "\n------------\n" ++ "" ++ show (slots !! 6) ++ "|" ++ show (slots !! 7) ++ "|" ++ show (slots !! 8) ++ "\n")
       pos <- getChar
       getChar
-      if notElem pos slots then do
-        putStrLn "Opsi option taken, try again!"
-        runGame slots 1
+      if notElem pos slots
+        then do
+          putStrLn "Opsi option taken, try again!"
+          runGame slots 1
         else runGame (newBoard slots turn pos) 0
 
---Receives a board, a  player and a position on the board.
+
+--Receives a board a player's turn and a position on the board.
 -- It returns a new board with the position
+-- Here we use guards | and otherwise. They are a syntax sugar for if/else statements. Otherwise workds as a  "default" expression
+-- if the 2 conditions are not met we call again newBoard but this time without the first index;
+-- we recursively do it untill one of the conditions are met and then we generate a board with X or O applied to it.
 newBoard :: Board -> Turn -> Option -> Board
-newBoard (x : xs) turn pos -- x is the first index of the list, the "head" xs is the rest of the list
-  | (x == pos) && (turn == 0) = ['X'] ++ xs
+newBoard (x : xs) turn pos -- x is the first index of the list xs is the rest of the list ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+ -- It reads like: Is the first index(head) of slots ['1', '2', '3', '4', '5', '6', '7', '8', '9'] in this case "1" equal to the value of pos and is the turn  equal to 0?
+  | (x == pos) && (turn == 0)  = ['X'] ++ xs
   | (x == pos) && (turn == 1) = ['O'] ++ xs
-  | otherwise = x : newBoard xs turn pos
+  | otherwise = x : newBoard xs turn pos  
 
 -- The I/O action allow us to read inputs from the user and print things the screen.
 -- Greets player and start/end game
